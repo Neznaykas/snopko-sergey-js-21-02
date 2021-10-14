9ccfde19ed93fd346fbd27dc4fa131df7025b135
sergey.snopko <sergey.snopko@gmail.com>
2021-10-14 21:49:31 +1000

    почти всё кроме дз 6, и 8

--- homework_04/main.js
+++ homework_04/main.js
@@ -1,6 +1,6 @@
 //1
 function dz1() {
-    const text = prompt("Введите две строки через запятую");
+    const text = prompt("Введите две строки через запятую", '123,123');
     let substr = text.split(',');
     alert(substr[0].indexOf(substr[1]) !== -1);
 }
@@ -19,7 +19,7 @@ function dz3() { //12/02/2021 12-00
     let text = prompt("Введите строку с датой формата: '12/02/2021 12-00'").trim();
 
     //громозко, но задание же на регулярные выражения
-    const regex = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4} (2[0-4]|[01][0-9])-([0-5][0-9])$/;
+    const regex = /^(0?[1-9]|[12]\d|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4} (2[0-4]|[01]\d)-([0-5]\d)$/;
 
     if (regex.test(text)) {
         text = text.replaceAll('\/', '.');
@@ -31,3 +31,40 @@ function dz3() { //12/02/2021 12-00
 
 }
 
+function dz4() { //Снопко Сергей Игоревич
+    const text = prompt("Введите строку с ФИО", "Снопко Сергей Игоревич").trim();
+    //тут всё стандартно
+    const regex = /^[А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+( [А-ЯЁ][а-яё]+(вна|вич))?$/;
+
+    alert(regex.test(text));
+}
+
+function dz5() {
+    let text = prompt("Введите строку (переменную) в стиле СamalCase", "TestKwestStr").trim();
+
+    const regex = /[A-Z|А-ЯЁ]/g;
+
+    const final = text.replace(regex,function (match,offset) {
+        if (offset > 0)
+            return "_" + match.toLowerCase();
+        else
+            return match.toLowerCase();
+    })
+
+    alert(final);
+}
+
+function dz6() {
+    //надо подумать
+}
+
+function dz7() {
+    const text = prompt("Введите строку (с числами)").trim();
+    const regex = /([0-9]+\.[0-9]+)|([0-9]+)/g;
+
+    alert(text.match(regex));
+}
+
+function dz8() {
+    //и тут надо подумать
+}
