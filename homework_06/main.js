ff4f77c8dade58a1fb56068d940293b44cc0fa30
sergey.snopko <sergey.snopko@gmail.com>
2021-10-22 22:01:57 +1000

    финальный

--- homework_06/main.js
+++ homework_06/main.js
@@ -41,4 +41,53 @@ function dz()
     console.log(fib2(25));
     console.log(fib2(50));
 
+    function toObject(array)
+    {
+        let obj = {};
+        array.forEach((elem) =>
+            {
+                if (Array.isArray(elem[1])) {
+                    obj[elem[0]] = toObject(elem[1]);
+                } else
+                    if (elem[1] === Object(elem[1]))
+                    {
+                        elem[1] = Object.entries(elem[1]);
+                        obj[elem[0]] = toObject(elem[1]);
+                    } else
+                        obj[elem[0]] = elem[1];
+            }
+        )
+        return obj;
+    }
+
+    //взято из чата в тг
+    const array = [
+        ['name1', 'Anna'],
+        ["key8", {
+            "key9": {"key10": [["key11","value11"],["key12","value12"]]}}],
+        ['age', 22],
+        ['pets',
+            [
+                ['dog', 'Faust'],
+                ['cat', 'Balthazar'],
+                ['cat2', 'Balthazar'],
+                ['object_test',
+                    {
+                        "key1": [
+                            ["key2", "value 2"],
+                            ["key3", "value 3"],
+                            ["key4", "value 4"]
+                        ],
+                        "key5": {
+                            "key6": "value6"
+                        }
+                    }
+                ]
+            ]
+        ]
+    ];
+
+    const ob = toObject(array);
+    console.log(ob);
+
 }
