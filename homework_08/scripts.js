0e457aaf839fbc2ee4f3139d33df7c35a755dff8
sergey.snopko <sergey.snopko@gmail.com>
2021-11-03 17:54:58 +1000

    добавление новых строк через валидируемые input-ы

--- homework_08/scripts.js
+++ homework_08/scripts.js
@@ -17,6 +17,38 @@ let add = document.getElementById("add");
 
 add.addEventListener('click', e =>
 {
+    const regex = /^[А-ЯЁ][а-яё]+/;
+    const regex2 = /\d/;
+
+    const text = document.getElementsByName('name')[0].value;
+    const number = document.getElementsByName('number')[0].value;
+
+    if  (regex.test(text) === false || regex2.test(number) === false)
+        alert('Введите корректные данные!');
+    else
+    {
+        let tbody = document.getElementById('table').getElementsByTagName('tbody')[0];
+
+        let row = tbody.insertRow();
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+
+        cell1.innerHTML = tbody.getElementsByTagName('tr').length.toString();
+        cell2.innerHTML = text;
+        cell3.innerHTML = number;
+        cell4.innerHTML = '<input type="button" value="Удалить" id="del">';
+
+        let btn = '<input type="button" value="Удалить" id="del">';
+
+        /*btn.addEventListener('click', e => {
+
+        });*/
+
+        cell4.innerHTML = btn;
+    }
+
 
 })
 
