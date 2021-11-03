b9a31c8e6485c8858e2aab7ed4d149588f472703
sergey.snopko <sergey.snopko@gmail.com>
2021-11-03 10:16:35 +1000

    всякие мелочи, переключение тем

--- homework_08/scripts.js
+++ homework_08/scripts.js
@@ -1,14 +1,31 @@
 function theme1()
 {
+    let table = document.querySelectorAll('td');
+
+    table.forEach(function (item) {
+        item.classList.remove('dark', 'orange')
+        item.classList.add('light');
+    });
 
 }
 
 function theme2()
 {
+    let table = document.querySelectorAll('td');
 
+    table.forEach(function (item) {
+        item.classList.remove('light', 'orange');
+        item.classList.add('dark');
+    });
 }
 
 function theme3()
 {
+    let table = document.querySelectorAll('td');
 
+    table.forEach(function (item) {
+        item.classList.remove('light', 'dark');
+
+        item.classList.add('orange');
+    });
 }
