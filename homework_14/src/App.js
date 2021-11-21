57bf329ec13a2dc717b3b3864e2add53ffc818e1
sergey.snopko <sergey.snopko@gmail.com>
2021-11-21 18:22:11 +1000

    начало работы над Todo-листом

---
+++ homework_14/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import logo from "./logo.svg";
+
+ function App() {
+          return (
+          <div className="App">
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+          <p>Список дел (ToDo лист) на React</p>
+          <p>1. Делать дела</p>
+
+          </header>
+          </div>
+          );
+      }
+
+export default App;
