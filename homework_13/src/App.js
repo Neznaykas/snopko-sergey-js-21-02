a8763f1c18cf55ffe5a77d744867fe8cbdfefa0a
sergey.snopko <sergey.snopko@gmail.com>
2021-11-21 11:51:15 +1000

    новые компоненты

--- homework_13/src/App.js
+++ homework_13/src/App.js
@@ -1,26 +1,43 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
 import {Header} from "./components/header/Header";
 import {Product} from "./components/product/Product";
+import {Sidebar} from "./components/sidebar/Sidebar";
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
+class App extends React.Component {
+  render() {
+        return (
+            <div className="App">
+                <Header/>
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+                <aside className="sidebar">
+                    <b>Морская рыба</b>
+                    <Sidebar id="shark" name="Акула"/>
+                    <Sidebar id="perch" name="Окунь"/>
+                    <Sidebar id="turbot" name="Акула"/>
+                    <Sidebar id="cod" name="Акула"/>
+                    <b>Пресноводная рыба</b>
+                    <Sidebar id="fish1" name="Акула"/>
+                    <Sidebar id="fish2" name="Акула"/>
+                    <Sidebar id="fish3" name="Речной угорь"/>
+                </aside>
 
-            <Product name="Палтус"/>
-            <Product name="Сёмга"/>
-            <Product name="Сом"/>
-            <Product name="Мойва"/>
-            <Product name="Сельдь"/>
-            <Product name="Тунец"/>
-      </header>
-    </div>
-  );
+                <main className="container">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                </main>
+
+
+                <Product name="Палтус"/>
+                <Product name="Сёмга"/>
+                <Product name="Сом"/>
+                <Product name="Мойва"/>
+                <Product name="Сельдь"/>
+                <Product name="Тунец"/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- homework_13/src/components/header/Header.jsx
+++ homework_13/src/components/header/Header.jsx
@@ -3,8 +3,18 @@ import './Header.css'
 
 export class Header extends React.Component {
     render() {
-        return <div className='header'>
-            "Интернет магазин "Не только красивое""
-        </div>
+        return <header className="header">
+            <h1>Интернет магазин "Не только красивое"</h1>
+            <div className="header__loader">
+                <img src="img/fish.svg" alt="Загрузка..."/>
+            </div>
+            <div className="header__loader2">
+                <img src="img/fish.svg" alt="Загрузка..."/>
+            </div>
+            <div className="header__theme">
+                <input type="checkbox" id="header__theme__input" onClick="change__theme();"/>
+                    <label className="header__theme__label" htmlFor="header__theme__input">Тёмная тема</label>
+            </div>
+        </header>
     }
 }
