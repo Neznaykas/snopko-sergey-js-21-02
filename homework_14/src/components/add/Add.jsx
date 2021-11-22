ead0287da5dca2fa42caef066fd1b17a4bd02ad2
Neznaykas <sergey.snopko@gmail.com>
2021-11-22 14:03:08 +1000

    промежуточный

--- homework_14/src/components/add/Add.jsx
+++ homework_14/src/components/add/Add.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import './Add.css'
+import {number} from "prop-types";
 
-export class Add extends React.Component {
+interface State { // Объявление интерфейса пропсов
+    text: string;
+}
+
+export class Add extends React.Component<State> {
+    //const [value,setValue] = useState("");
+
+    constructor(props) {
+        super(props);
+        this.change = this.change.bind(this);
+        this.state.text = ''
+    }
+
+    change(item) {
+       console.log(item.target.value);
+        //item.target.value
+
+       this.setState({text: this.state});
+    }
+
+    add()
+    {
+        console.log('append');
+    }
+
     render() {
         return <section className="container__item">
             <div className="container__append">
-                <textarea>Заметка</textarea>
-                <input type="button" value="Добавить"/>
+                <textarea value={this.state.text} onChange={this.change}/>
+                <input type="button" value="Добавить" onClick={this.add}/>
             </div>
         </section>
     }
--- homework_14/src/components/table/Table.jsx
+++ homework_14/src/components/table/Table.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import './Table.css'
+import PropTypes, {number} from 'prop-types'
 
-export class Table extends React.Component {
+interface Props { // Объявление интерфейса пропсов
+    id?: number;
+    text?: string;
+    remove?: () => void;
+}
+
+export class Table extends React.Component<Props> {
     render() {
         return <section className="container__item">
             <div className="container__item__item">
