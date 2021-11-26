79e9a6fa9ed0ef4156cf7c272934a536e816d9fb
Neznaykas <sergey.snopko@gmail.com>
2021-11-26 10:14:14 +1000

    dumMyApi из урока

---
+++ homework_15/src/api/dumMyApi.ts
@@ -0,0 +1,20 @@
+import {
+    APP_ID_FIELD, APP_ID_VALUE, COMMENT_URL, LIMIT_FIELD, PAGE_FIELD,
+} from '../constants/dumMyApi';
+import { CommentType, PostListResponse } from '../types/dumMyApiResponses';
+
+export const getCommentsList = (
+    page: number,
+    limit: number,
+    callback: (resp: Array<CommentType>) => void,
+    errorCallback?: (resp: any) => void,
+) => fetch(COMMENT_URL, {
+    method: 'GET',
+    headers: new Headers({
+        [APP_ID_FIELD]: APP_ID_VALUE,
+        [PAGE_FIELD]: page.toString(),
+        [LIMIT_FIELD]: limit.toString(),
+    }),
+}).then((response) => response.json())
+    .then((response: PostListResponse) => callback(response.data))
+    .catch(errorCallback);
